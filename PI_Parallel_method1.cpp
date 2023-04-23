a33c98bbc31fd8a70de425b3c1f143608ad17bc9
Ahmed Elkhatib <131554633+el5hatib@users.noreply.github.com>
2023-04-23 07:22:33 +0200

    Calculate PI Parallel Code method 1

---
+++ PI_Parallel_method1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <omp.h>
+using namespace std;
+int main()
+{
+    int steps = 10000000000;
+    double rate = 1.0 / steps;
+    double pi = 0.0;
+    double sum[3] = {0,0,0};
+    float stime = omp_get_wtime();
+#pragma omp parallel num_threads(3)
+    {
+        int id = omp_get_thread_num();
+        int threadsn = omp_get_num_threads();
+        for (int i = id; i < steps; i += threadsn)
+        {
+            float x = ((i + 0.5) * rate);
+            sum[id] += (4.0 / (1.0 + x * x));
+        }
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        pi = pi + sum[i] * rate;
+    }
+    float etime = omp_get_wtime();
+    float takentime = etime - stime;
+    cout << "The  Value Of PI = " << pi << endl;
+    cout << "Taken Time =  " << takentime << endl;
+    return 0;
+}
