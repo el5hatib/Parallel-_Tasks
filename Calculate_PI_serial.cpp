f136c321dddcbb7733e0935c79fcfc7624fceaba
Ahmed Elkhatib <131554633+el5hatib@users.noreply.github.com>
2023-04-23 07:19:53 +0200

    Calculate PI Serial Code

---
+++ Calculate_PI_serial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <omp.h>
+using namespace std;
+int main()
+{
+	int steps = 10000;
+	float rate = 1.0 / steps;
+	float x; 
+	float sum = 0;
+	float stime = omp_get_wtime();
+	for (int i = 0; i < steps; i++)
+	{
+		x = (i + 0.5) * rate; 
+		sum = sum + (4 / (1 + x * x)); 
+
+	}
+	float pi = sum /steps;
+	float etime = omp_get_wtime();
+	float takentime = etime - stime; 
+	cout << "The Value Of PI =  " << pi << endl; 
+	cout << "Taken Time = " << takentime << endl; 
+	return 0;
+}
