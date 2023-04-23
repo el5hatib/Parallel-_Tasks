be84f630645b660ce31481998eae81b8c70b35aa
Ahmed Elkhatib <131554633+el5hatib@users.noreply.github.com>
2023-04-23 05:37:44 +0200

    factorial_of_N_nmbers_method1

---
+++ factorial_of_N_nmbers_method1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+int main()
+{
+    long n;
+    int nthreads;
+
+    cout << "Please enter the number of integers to fact: " << endl;
+    cin >> n;
+
+    long fact[2] = { 1, 1 };
+    double STime = omp_get_wtime();
+#pragma omp parallel num_threads(2)
+    {
+        int id = omp_get_thread_num();
+        nthreads = omp_get_num_threads();
+
+        for (int i = id + 1; i <= n; i += nthreads)
+        {
+            fact[id] *= i;
+        }
+
+
+    }
+    long result = 1;
+
+    for (int i = 0; i <2; i++)
+    {
+        result *= fact[i];
+    }
+    double ETime = omp_get_wtime();
+    double TakenTime = ETime - STime;
+    cout << "The fact of the  " << n << " integers is: " << result << endl;
+    cout << "Taken Time :  =  " << TakenTime << endl;
+    return 0;
+}
