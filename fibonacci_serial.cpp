e93c782552d60b343de556b81d5ba776c67bac88
Ahmed Elkhatib <131554633+el5hatib@users.noreply.github.com>
2023-04-25 11:07:11 +0200

    computes the n Fibonacci number serial code

---
+++ fibonacci_serial.cpp
@@ -0,0 +1,33 @@
+#include  <iostream>
+#include <omp.h>
+using namespace  std;
+/*
+ * Fibonacci function that computes the n Fibonacci number serial code
+ * 
+ */
+int fibonacci(int n)
+{
+		int i, j;
+if (n<2)
+	return 1;
+else
+{
+	i = fibonacci(n-1);
+	j = fibonacci(n-2);
+	return i+j;
+}
+}
+int main()
+{
+
+int n = 45;
+	int result;
+	double start, end;
+	start = omp_get_wtime();
+	result = fibonacci(n);
+	end = omp_get_wtime();
+	cout << "Fibonacci(" << n << ") = " << result << endl;
+	cout << "Time: " << end - start << endl;
+	return 0;
+
+}
